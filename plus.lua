8a2ef0a4b874cd9523afa147318bca1e488a70ab
jordanfb <jmanfb@gmail.com>
2019-08-18 17:35:42 -0400

    Added support for no ender chests in plus

--- plus.lua
+++ plus.lua
@@ -26,33 +26,106 @@ end
 
 dist = args[1]
 torch = 10
+numberOfChests = turtle.getItemCount(16) -- if there's something in the last slot we're assuming it's a chest
+isEnderChest = false
 
 local avoidList = { -- a list of the blocks the turtle shouldn't mine.
-  {"minecraft:grass", 0}, -- block name and block metadata
+  {"minecraft:grass", 0}, -- block name and block metadata, if metadata == -1 then it ignores all values
   {"minecraft:dirt", 0},
   {"minecraft:stone", 0},
   {"minecraft:cobblestone", 0},
   {"minecraft:gravel", 0},
-  {"minecraft:bedrock", 0}
+  {"minecraft:bedrock", 0},
+  {"minecraft:stone", 1},
+  {"minecraft:stone", 2},
+  {"minecraft:stone", 3},
+  {"minecraft:lava", -1),
 }
+
+local trashList = { -- items to drop from its inventory onto the ground if it can't put it in a chest nicely
+  {"minecraft:grass", 0},
+  {"minecraft:dirt", 0},
+  {"minecraft:stone", 0},
+  {"minecraft:cobblestone", 0},
+  {"minecraft:gravel", 0},
+  {"minecraft:bedrock", 0},
+  {"minecraft:stone", 1},
+  {"minecraft:stone", 2},
+  {"minecraft:stone", 3},
+  {"minecraft:lava", -1)
+}
+
+function checkIfEnderChest()
+  -- this function is used to check if the turtle has a regular chest or an ender chest  
+  detail = turtle.getItemDetail(16)
+  -- do something with the chest to figure out if it's an ender chest or some regular chest :P
+  if detail == nil then
+    hasChest = false
+    print("Has no chests")
+  else
+    hasChest = true
+    isEnderChest = detail.name == "enderstorage:ender_storage"
+    if isEnderChest then
+      print("Has ender chest")
+    else
+      print("Has regular chest")
+    end
+  end
+end
 
 function emptyInventory()
   turtle.select(16)
-  while not turtle.placeUp() do
-    turtle.digUp()
+  while not turtle.placeDown() do
+    turtle.digDown()
   end
   for i = 2, 16 do -- start at 2 because 1 is the torches
     turtle.select(i)
-    turtle.dropUp()
+    turtle.dropDown()
   end
-  turtle.select(16)
-  turtle.digUp()
+  if isEnderChest then
+    turtle.select(16)
+    turtle.digDown()
+  end
   turtle.select(1)
+  numberOfChests = turtle.getItemCount(16) -- update the count
 end
 
-function emptyIfNecessary()
+function dumpUnecessaryItems()
+  -- empty things like cobble and dirt
+  for inventory = 1, 16 do
+    bool, block = turtle.getItemDetail(inventory)
+    if bool then
+      local trash = false
+      for i = 1, #trashList do
+        if block.name == trashList[i][1] and (block.metadata == trashList[i][2] or trashList[i][2] == -1) then
+          turtle.select(inventory)
+          turtle.drop()
+          break
+        end
+      end
+    end
+  end
+end
+
+function organizeInventory()
+  -- move everything into the earliest inventory slot possible so that we know when we're full
+  for i = 15, 2, -1 do
+    turtle.select(i)
+    for j = 2, i do
+      turtle.transferTo(j) -- try to transfer them to that slot, this should combine them if there's room
+    end
+  end
+end
+
+function emptyIfNecessary(onMainPath)
   if turtle.getItemCount(15) > 0 then
-    emptyInventory()
+    if (not isEnderChest and not onMainPath) or not hasChest then
+      -- don't empty it but try to get rid of unimportant things, it doesn't empty because it may run into it again on the recursive route back and we don't want that...
+      dumpUnecessaryItems()
+    else
+      emptyInventory()
+    end
+    organizeInventory()
   end
 end
 
@@ -60,20 +133,20 @@ function avoidDig()
   local bool, block = turtle.inspect()
   local avoid = false
   for i = 1, #avoidList do
-    if block.name == avoidList[i][1] and block.metadata == avoidList[i][2] then
+    if block.name == avoidList[i][1] and (block.metadata == avoidList[i][2] or avoidList[i][2] == -1) then
       avoid = true
     end
   end
   if not avoid then
     turtle.dig()
-    emptyIfNecessary()
+    emptyIfNecessary(false)
   end
 end
 
 function goUp()
   while not turtle.up() do
     turtle.digUp()
-    emptyIfNecessary()
+    emptyIfNecessary(false)
   end
   z = z + 1
 end
@@ -81,7 +154,7 @@ end
 function goDown()
   while not turtle.down() do
     turtle.digDown()
-    emptyIfNecessary()
+    emptyIfNecessary(false)
   end
   z = z - 1
 end
@@ -103,7 +176,7 @@ end
 function goForward()
   while not turtle.forward() do
     turtle.dig()
-    emptyIfNecessary()
+    emptyIfNecessary(false)
   end
   if facing == 0 then -- north
     y = y + 1
@@ -135,7 +208,7 @@ function goBackwards()
 end
 
 function recursiveDig(originFacing, originMove, recursiveLevel) -- move is up, down, forwards, so it un-turns and then un moves.
-  emptyIfNecessary()
+  emptyIfNecessary(false)
   if originMove == 3 then -- try digging in front of you
     local bool, block = turtle.inspect()
     local avoid = not bool -- if it doesn't exist, avoid it.
@@ -147,7 +220,7 @@ function recursiveDig(originFacing, originMove, recursiveLevel) -- move is up, d
     if not avoid then
       while turtle.detect() do
         turtle.dig()
-        emptyIfNecessary()
+        emptyIfNecessary(false)
       end
     else
       return -- not an ore to mine so return
@@ -165,7 +238,7 @@ function recursiveDig(originFacing, originMove, recursiveLevel) -- move is up, d
     if not avoid then
       while turtle.detectUp() do
         turtle.digUp()
-        emptyIfNecessary()
+        emptyIfNecessary(false)
       end
     else
       return-- not an ore to mine so return
@@ -183,7 +256,7 @@ function recursiveDig(originFacing, originMove, recursiveLevel) -- move is up, d
     if not avoid then
       while turtle.detectDown() do
         turtle.digDown()
-        emptyIfNecessary()
+        emptyIfNecessary(false)
       end
     else
       return -- not an ore to mine so return
@@ -230,13 +303,14 @@ end
 function sureDig()
   while turtle.detect() do
     turtle.dig()
-    emptyIfNecessary()
+    emptyIfNecessary(false)
   end
 end
 
 function main()
   for j = 1, reps do
     for i = 0, dist do
+      emptyIfNecessary(true)
       sureDig()
       goForward()
       recursiveDig(facing, 1, 1) -- dig up
@@ -286,4 +360,7 @@ function main()
   end
 end
 
+
+
+checkIfEnderChest()
 main()

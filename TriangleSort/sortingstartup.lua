572e581c40d99707f8484fe6dfffe85fe537ee31
Jordan Faas-Bush <jmanfb@gmail.com>
2019-12-20 23:24:37 -0500

    Adjusted example startup code to work with cloning the repo to the turtle

--- TriangleSort/sortingstartup.lua
+++ TriangleSort/sortingstartup.lua
@@ -1,2 +1,3 @@
 print(os.getComputerID() ..  " " .. os.getComputerLabel())
-shell.run("stuffmanager.lua")
+-- shell.run("TriangleSort.lua")
+shell.run("ComputercraftCode/TriangleSort/TriangleSort.lua") -- this works if I've cloned the repo onto the turtle

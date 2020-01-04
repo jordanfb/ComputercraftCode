235f796ff94440ce1e2481a452e63a9ee552fd56
Jordan Faas-Bush <jmanfb@gmail.com>
2020-01-04 02:47:02 -0500

    Typo non-lua or symbol

--- TriangleSort/TriangleSort.lua
+++ TriangleSort/TriangleSort.lua
@@ -293,7 +293,7 @@ end
 
 function paged_print_all_stored_items(alphabetical)
 	-- defaults to alphabetical
-	if alphabetical == nil || alphabetical == true then
+	if alphabetical == nil or alphabetical == true then
 		local output = ""
 		for display_name, item_key in itemKeyAlphabeticallyByDisplayName() do
 			if items_stored[item_key] ~= nil then
